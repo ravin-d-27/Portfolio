df845c4fcb00fde1d96fb27591418a79759d0d15
Ravin D <ravin.d3107@outlook.com>
2023-12-08 19:02:17 +0530

    Changed the database to Postgres

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django.db.backends.sqlite3
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,11 +77,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('postgres://ravin_d_27_database_user:gDLctwhOQgfVdkDtz40cXxlyrwcQMldT@dpg-clphkcp46foc73cgdr20-a.singapore-postgres.render.com/ravin_d_27_database')
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
